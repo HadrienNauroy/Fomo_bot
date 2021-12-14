4ee50be01c3c427a15e1f8de8d1c75c4075e5cad
HadrienNauroy <hadrien.nauroy@ensta-paris.fr>
2021-12-14 16:34:30 +0100

    Add config_exemple.y
    Add an exemple a config file

---
+++ config_exemple.py
@@ -0,0 +1,6 @@
+API_KEY = "your_api_key"
+API_SECRET = "your_api_secret"
+USR = "fb_username"
+PWD = "fb_password"
+ID = "your_messenger_ID"
+UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
